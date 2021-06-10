74f21e361b3787731e5ac5b9f2ac5ce3c186ab81
Oleg Egorov <egorov@ylab.io>
2021-06-10 16:08:17 +0400

    Change USDT

--- wallet_usdt.go
+++ wallet_usdt.go
@@ -9,7 +9,7 @@ type usdt struct {
 }
 
 func newUSDT(key *Key) Wallet {
-	token := newBTC(key).(*eth)
+	token := newETH(key).(*eth)
 	token.name = "Tether"
 	token.symbol = "USDT"
 	token.contract = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
